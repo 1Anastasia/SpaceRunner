7f07d1cec2ef75a25ee294ce74ecb7c5bafda6cb
1Anastasia <arnika@1cv8.org.ua>
2017-01-19 18:52:28 +0200

    +

---
+++ Assets/Scripts/BackgroundControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundControler: MonoBehaviour
+{
+
+    public float partSitze;
+    public float speed;
+    public List<Transform> parts;
+
+    void Start()
+    {
+        for (var i = 0; i < transform.childCount; ++i)
+        {
+            parts.Add(transform.GetChild(i));
+        }
+        for (var i = 0; i < parts.Count; ++i)
+        {
+            var pos = Vector3.zero;
+            pos.y = partSitze * i;
+            parts[i].transform.position = pos;
+        }
+    }
+
+    void Update()
+    {
+        foreach (var part in parts)
+        {
+            var pos = part.position;
+            pos.y -= speed * Time.deltaTime;
+            if (pos.y <= -partSitze)
+            {
+                pos.y = partSitze * (parts.Count - 1);
+            }
+            part.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed = 0.1F;
+
+    private void Update()
+    {
+        transform.Translate(0f, -speed * Time.deltaTime, 0f);
+        if (transform.position.y < -8f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
