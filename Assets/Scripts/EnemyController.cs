ed2229eee705ba239c80c068ab44534917bc9173
1Anastasia <arnika@1cv8.org.ua>
2017-01-26 21:29:50 +0200

    ShotEnemy
    стрельба врага

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,6 +7,14 @@ public class EnemyController : MonoBehaviour
 
     public float speed = 1f;
     public float widthBG = 5f;
+    public List<GameObject> shots;
+    public float delay = 5f;
+    public float speedS = 2F;
+
+    void Start()
+    {
+        StartCoroutine(Generator());
+    }
 
     void Update()
     {
@@ -14,6 +22,18 @@ public class EnemyController : MonoBehaviour
         if (transform.position.y < -8f)
         {
             Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Generator()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            var shot = Instantiate(shots[Random.Range(0, shots.Count)]);
+            shot.transform.SetParent(transform, false);
+            var deltaY = speed * Time.deltaTime;
+            shot.transform.localPosition = new Vector3(0, deltaY, 0);
         }
     }
 }
